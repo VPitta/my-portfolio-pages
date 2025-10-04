0625d3df23c825185a1ed95f47d17c0873c51162
Vpitta <vinicius.pita.06@gmail.com>
2025-10-04 10:17:54 -0300

    atualizando icons

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/VPitta/my-portfolio-pages/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/VPitta/my-portfolio-pages/refs/heads/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
